d174e75479eb88b2b97cea407e78739b1c4bc5f7
A1K2S3 <senghaniadarshkishor1.9@gmail.com>
2019-01-25 21:36:12 +0530

    Java

---
+++ Exp 10/constant.java
@@ -0,0 +1,15 @@
+class constant{
+
+     constant(){
+
+          System.out.println("Constructor Working!!");
+          
+     }
+     
+     public static void main( String args[] ) {
+
+          constant o1 = new constant();
+          
+     }
+     
+}
