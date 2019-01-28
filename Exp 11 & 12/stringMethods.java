d4474d93ff3d2f3c74c7bcec1be9f84b45d44075
A1K2S3 <senghaniadarshkishor1.9@gmail.com>
2019-01-28 08:18:49 +0530

    Java

---
+++ Exp 11 & 12/stringMethods.java
@@ -0,0 +1,19 @@
+class stringMethods{
+
+     public static void main( String args[] ) {
+          
+          // charAt(index)
+          String str = "Java Programming";
+          char index = str.charAt(3);
+          System.out.println(index + " // charAt(index)");
+
+          // boolean equals(object object2)
+          String str2 = "Java Programming";
+          boolean value = equals(str str2);
+          System.out.println(value + " // equals(object object2)");
+
+
+          
+     }
+     
+}
